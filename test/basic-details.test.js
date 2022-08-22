b3f46494200ff1efc8461e6969cc823b039bb2b1
Joyel J Thomas <joyel97j@gmail.com>
2022-08-22 11:11:01 +0530

    Final commit

--- test/basic-details.test.js
+++ test/basic-details.test.js
@@ -1,8 +1,35 @@
 import { html, fixture, expect } from '@open-wc/testing';
-import { stub } from 'sinon';
+import Sinon, { stub } from 'sinon';
 import '../src/LoanBasicDetails/BasicDetails.js';
 
+const el = await fixture(html`<basic-details></basic-details>`);
+const myFuctionStub = Sinon.stub(el, '_captureDetails');
+
+
 describe('Basic details', () => {
-  // Write test cases inside this block
-  // refer basic-details.js files
+
+   it('check for accessebility', () => {
+        expect(el).to.be.accessible;
+    });
+
+    it('capturedetails click button', async() => {
+        el.requestUpdate();
+        await el.updateComplete;
+        el.shadowRoot.querySelector('lion-button').click();
+        expect(myFuctionStub).to.have.callCount(0);
+    });
+
+    it('toDashboard click button', async () => {
+        const myFunction = Sinon.stub(el, '_toDashboard');
+        el.requestUpdate();
+        await el.updateComplete;
+        el.shadowRoot.querySelector('lion-button').click();
+        expect(myFunction).to.have.callCount(1);
+    });
+
+    it('check for id', async() => {
+        const el = await fixture('<div id = "word"></div>');
+        expect(el.id).to.equal('word');
+    });
+
 });
--- test/loan-application.test.js
+++ test/loan-application.test.js
@@ -1,7 +1,34 @@
 import { html, fixture, expect } from '@open-wc/testing';
-
+import * as Sinon from 'sinon';
 import '../loan-application.js';
 
+let el;
+
 describe('LoanApplication', () => {
-  // Write test cases inside this block
+    before(async () => {
+        el = await fixture(html`<loan-application></loan-application>`);
+    });
+
+    it('Accessible', async () => {
+        expect(el).to.be.accessible();
+    });
+
+    it('Check for the title type', async () => {
+        el = await fixture(html`<dash-board></dash-board>`);
+        expect(el.title).to.be.a('string');
+    });
+
+    it('Check for the title value', async () => {
+        el = await fixture(html`<dash-board></dash-board>`);
+        expect(el).shadowDom.to.equal('<loan-header></loan-header><div id = "outlet"></div>')
+    });
+
+    it('spy the increment function', () => {
+        setTimeout(async () => {
+            const spy = Sinon.stub(el, counter);
+            expect(spy.callCount).to.equal();
+        }, 3000);
+    })
+
+    
 });
